e8d6916f545cbd84ba6251f79ce8d50e90b65e83
orlenok777 <47170724+orlenok777@users.noreply.github.com>
2024-06-30 10:41:52 +0000

    chenge  2

--- src/VoiceReminder.jsx
+++ src/VoiceReminder.jsx
@@ -114,7 +114,7 @@ const VoiceReminder = () => {
             startIcon={<MicIcon />}
             fullWidth
           >
-            Установить напоминание 7
+            Установить напоминание 8
           </Button>
         </div>
         {reminders.length > 0 && (
